ffbafbe80236adc64219aa248514fffe7e736db6
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-11-21 11:28:17 +0530

    story cahnges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { TiTick } from "react-icons/ti";
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
 import { CiShare1 } from "react-icons/ci";
 import Notification from './Notification';
+import Story from './Story';
 
 const Navbar = () => {
 
@@ -12,6 +13,16 @@ const Navbar = () => {
 
     const showNotifPage = () => {
         setShowNotification(true);
+    };
+
+    const [story, setStory] = useState(null);
+
+    const openStory = (img, name) => {
+        setStory({ img, name });
+    };
+
+    const closeStory = () => {
+        setStory(null);
     };
 
     const avatarr = [
@@ -22,7 +33,7 @@ const Navbar = () => {
         { id: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Chris_Evans_at_the_2025_Toronto_International_Film_Festival_%28cropped%29.jpg/250px-Chris_Evans_at_the_2025_Toronto_International_Film_Festival_%28cropped%29.jpg', name: 'Raju' },
         { id: 6, img: 'https://img.etimg.com/thumb/width-1200,height-1200,imgsize-80284,resizemode-75,msid-90911048/magazines/panache/kgf-chapter-2-hindi-to-break-record-of-baahubali-2-and-become-the-fastest-film-to-enter-rs-200-cr-club.jpg', name: 'Roshan' },
         { id: 7, img: 'https://www.bollywoodhungama.com/wp-content/uploads/2022/04/4fed31f9-4214-4c0d-a5d7-e6753119640f.jpg', name: 'Yash' },
-    ]
+    ];
 
     const postsData = [
         {
@@ -52,19 +63,24 @@ const Navbar = () => {
             likes: 500,
             bg: "https://images.pexels.com/photos/15036815/pexels-photo-15036815.jpeg"
         }
-    ]
+    ];
 
-    const [posts, setPosts] = useState(postsData)
+    const [posts, setPosts] = useState(postsData);
 
     const toggleLike = (id) => {
         setPosts(prev =>
             prev.map(p =>
                 p.id === id ? { ...p, likes: p.liked ? p.likes - 1 : p.likes + 1, liked: !p.liked } : p
-            ))
-    }
+            )
+        );
+    };
 
     if (showNotification) {
         return <Notification />;
+    }
+
+    if (story) {
+        return <Story img={story.img} name={story.name} closeBitton={closeStory} />;
     }
 
     return (
@@ -82,8 +98,12 @@ const Navbar = () => {
             <div className='flex gap-6 mt-8 px-4 overflow-x-auto no-scrollbar justify-center'>
                 {avatarr.map((item, index) => (
                     <div key={index} className='shrink'>
-                        <img className='rounded-full p-1 w-[70px] h-[70px] md:w-[95px] md:h-[95px] border-4 border-red-600 cursor-pointer'
-                            src={item.img} alt="" />
+                        <img
+                            onClick={() => openStory(item.img, item.name)}
+                            className='rounded-full p-1 w-[70px] h-[70px] md:w-[95px] md:h-[95px] border-4 border-red-600 cursor-pointer'
+                            src={item.img}
+                            alt=""
+                        />
                         <p className='text-center text-sm mt-2'>{item.name}</p>
                     </div>
                 ))}
@@ -91,12 +111,15 @@ const Navbar = () => {
 
             {posts.map(post =>
                 <div key={post.id} className='mt-8'>
-
-                    <div onDoubleClick={() => toggleLike(post.id)} className='w-full h-[350px] md:h-[550px] bg-cover bg-center back_img' style={{ backgroundImage: `url(${post.bg})` }} >
+                    <div onDoubleClick={() => toggleLike(post.id)}
+                        className='w-full h-[350px] md:h-[550px] bg-cover bg-center back_img'
+                        style={{ backgroundImage: `url(${post.bg})` }} >
 
                         <div className='flex gap-2 p-4'>
-                            <img className='rounded-full w-[60px] h-[60px] md:w-[75px] md:h-[75px] border-4 border-red-600 cursor-pointer'
-                                src={post.profileImg} /> 
+                            <img
+                                className='rounded-full w-[60px] h-[60px] md:w-[75px] md:h-[75px] border-4 border-red-600 cursor-pointer'
+                                src={post.profileImg}
+                            />
                             <div>
                                 <p className='flex items-center text-white text-lg md:text-xl'>
                                     {post.name}
@@ -108,7 +131,6 @@ const Navbar = () => {
                     </div>
 
                     <div className='flex text-2xl gap-8 mt-4 px-5 md:px-20 lg:px-40 ml-[50px]'>
-
                         <p onClick={() => toggleLike(post.id)} className='flex items-center gap-2 cursor-pointer'>
                             <span className='text-sm font-black'>{post.likes}</span>
                             {post.liked ? <p className='text-red-600'><FaHeart /></p> : <FaRegHeart />}
@@ -124,7 +146,7 @@ const Navbar = () => {
                 </div>
             )}
         </>
-    )
-}
+    );
+};
 
 export default Navbar;
